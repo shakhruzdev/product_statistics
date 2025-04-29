8b38556ac11677b4a56389ca095883f612e03519
asus <159557381+shakhruzdev@users.noreply.github.com>
2025-04-29 17:26:51 +0500

    changes in models.py: Added Models changes in admin.py: Models registered

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Product,
+    Order
+)
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'phone_number')
+    list_display_links = ('id', 'full_name', 'phone_number')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'bought_price', 'sold_price', 'quantity')
+    list_display_links = ('id', 'name', 'bought_price', 'sold_price', 'quantity')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'product', 'quantity')
+    list_display_links = ('id', 'client', 'product', 'quantity')
--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    BigIntegerField,
+    CharField,
+    ForeignKey,
+    CASCADE,
+    IntegerField
+)
 
-# Create your models here.
+
+class User(Model):
+    full_name = CharField(max_length=255)
+    phone_number = CharField(max_length=12)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Product(Model):
+    name = CharField(max_length=255)
+    bought_price = BigIntegerField()
+    sold_price = BigIntegerField()
+    quantity = IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(Model):
+    client = ForeignKey('apps.User', on_delete=CASCADE)
+    product = ForeignKey('apps.Product', on_delete=CASCADE)
+    quantity = IntegerField()
+
+    def __str__(self):
+        return self.product.name
