996b6cda514aad7f94f826c1eebb8b7ec9017a0d
sarvar-py-dev <sarvarbekdavranbekov@gmail.com>
2025-04-30 10:50:22 +0500

    added urls

--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from apps.views import ProductCreateUpdateListView
+from apps.views import ProductCreateUpdateListView, ClientCreateListView, OrderCreateListView
 
 urlpatterns = [
-    path('', ProductCreateUpdateListView.as_view(), name='product_list'),
+    path('', ClientCreateListView.as_view(), name='client_list'),
+    path('products/', ProductCreateUpdateListView.as_view(), name='product_list'),
+    path('orders/', OrderCreateListView.as_view(), name='order_list'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,16 +1,21 @@
 from django.views.generic import ListView, CreateView
 
-from apps.models import Client, Product
+from apps.models import Client, Product, Order
 
 
 class ClientCreateListView(CreateView, ListView):
     model = Client
     template_name = 'clients.html'
+    fields = '__all__'
     context_object_name = 'clients'
 
-class ProductCreateUpdateListView (CreateView, ListView):
+
+
+class ProductCreateUpdateListView(CreateView, ListView):
     model = Product
     template_name = 'products.html'
 
 
-
+class OrderCreateListView(CreateView, ListView):
+    model = Order
+    template_name = 'orders.html'
