fc3cf3b5f12902301ad304a6cab6f2d48cff1c61
sarvar-py-dev <sarvarbekdavranbekov@gmail.com>
2025-05-07 17:32:59 +0500

    changed view name

--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from apps.views import (
-    ProductCreateUpdateListView,
+    ProductCreateListView,
     ClientCreateListView,
     OrderCreateListView,
     ClientDeleteView,
@@ -10,7 +10,7 @@ from apps.views import (
 
 urlpatterns = [
     path('', ClientCreateListView.as_view(), name='client_list'),
-    path('products/', ProductCreateUpdateListView.as_view(), name='product_list'),
+    path('products/', ProductCreateListView.as_view(), name='product_list'),
     path('orders/', OrderCreateListView.as_view(), name='order_list'),
     path('clients/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
     path('clients/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
